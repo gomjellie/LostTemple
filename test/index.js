ae4c8366bcef9fc80a5c3d753a984f0389403629
gomjellie <gomjellie@gmail.com>
2018-12-16 22:13:54 +0900

    테스트케이스 2개 작성

--- test/index.js
+++ test/index.js
@@ -1,3 +1,27 @@
 /**
  * test files on examples directory
  */
+
+const expect = require("chai").expect;
+
+const fs = require("fs");
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+
+describe('lost-temple', function(){
+    const lostTemple = require('../lib');
+
+    it('check gugu.cat -> gugu.c', function(){
+        readFile("./example/gugu.cat")
+            .then(lostTemple.cat2c)
+            .then((res) => { expect(res).to.equal(String(fs.readFileSync("./example/gugu.c")))})
+            .catch(console.log);
+    });
+
+    it('check helloWorld.cat -> helloWorld.c', function(){
+        readFile("./example/helloWorld.cat")
+            .then(lostTemple.cat2c)
+            .then((res) => { expect(res).to.equal(String(fs.readFileSync("./example/helloWorld.c")))})
+            .catch(console.log);
+    });
+});
