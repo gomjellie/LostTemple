bae0c43a85d59f277727998ffd02a21f4701cb43
gomjellie <gomjellie@gmail.com>
2018-12-18 18:35:12 +0900

    cli command change output required -> optional

--- example/guguFunction.c
+++ example/guguFunction.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+;
 int main() {
+    printGugu();
+}
+
+    return 0;
+}
+
+void printGugu() {
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            printf("%d times %d is %d \n", i, j, i * j);
+            print("%d times %d is %d \n", i, j, i * j);
         }
     }
 }
+
+;
