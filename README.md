77b576b1d85a1d6eea08778b28fa4e9f9fefe14a
gomjellie <gomjellie@gmail.com>
2021-04-17 21:16:09 +0900

    add calculator tokenizer

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## 이게 뭐죠?
 
-LostTemple 은 C언어를 파이썬 처럼 쓰게 도와주는 트랜스파일러 입니다.
+LostTemple 은 Javascript를 파이썬 처럼 쓰게 도와주는 트랜스파일러 입니다.
 
 ![바람직한 코딩스타일 예제](DOCS/img/brace_style.png)
 
@@ -14,37 +14,25 @@ LostTemple 은 C언어를 파이썬 처럼 쓰게 도와주는 트랜스파일�
 
 위의 사진처럼 다른언어에서도 파이썬처럼 { } 와 ; 없이 코드를 짜고 싶어집니다.
 
-```c
-#include <stdio.h>
+```js
 
-int main():
-    for (int i = 0; i < 10; i++):
-        for (int j = 0; j < 10; j++):
-            print("%d times %d is %d \n", i, j, i * j)
-
-    return 0
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
 
 ```
 
 LostTemple 을 사용하여 트랜스파일하면 아래와 같이 변합니다!
       ↓ ↓ ↓ ↓ ↓ ↓
 
-```c
-#include <stdio.h>
-
-int main() {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            printf("%d times %d is %d \n", i, j, i * j);
-        }
-    }
-
-    return 0;
-}
+```js
+for (let i = 0; i < 10; i++)               {
+  for (let j = 0; j < 10; j++)             {
+    console.log(`${i} * ${j} = ${i * j}`)  ;}}
 
 ```
-
-와 같은 형식으로 변환 시킬 수 있습니다.
 
 ## 사용법
 
@@ -56,14 +44,14 @@ npm install lost-temple -g
 
 ### 트랜스파일 하기
 
-아래의 3가지 방법으로 hello.cat 을 hello.c로 트랜스파일 할 수 있습니다.
+아래의 3가지 방법으로 hello.js를 트랜스파일 할 수 있습니다.
 
 ```sh
 
-lost hello.cat
+lost hello.js
 
-lost hello.cat -o hello.c
+lost hello.js -o hello.py.js
 
-lost hello.cat --output hello.c
+lost hello.js --output hello.py.js
 
 ```
--- test/index.js
+++ test/index.js
@@ -3,31 +3,26 @@
  */
 
 const expect = require("chai").expect;
-
 const fs = require("fs");
-const util = require('util');
 
 describe('lost-temple', function () {
   const lostTemple = require('../lib');
 
-  // it('check gugu.cat -> gugu.c', function (done) {
-  //   readFile("./example/gugu.cat")
-  //     .then(lostTemple.cat2c)
-  //     .then((res) => {
-  //       expect(res).to.equal(String(fs.readFileSync("./example/gugu.c")));
-  //       done();
-  //     })
-  //     .catch(console.error);
-  // });
+  it("check tokenizer.txt tokenize", function (done) {
+    const source = fs.readFileSync(`${__dirname}/sources/tokenizer.txt`).toString();
+    const tokens = lostTemple.tokenizer(source);
 
-  // it('check helloWorld.cat -> helloWorld.c', function (done) {
-  //   readFile("./example/helloWorld.cat")
-  //     .then(lostTemple.cat2c)
-  //     .then((res) => {
-  //       expect(res).to.equal(String(fs.readFileSync("./example/helloWorld.c")));
-  //       done();
-  //     })
-  //     .catch(console.error);
-  // });
+    expect(tokens).to.eql([
+      { type: 'paren', value: '(' },
+      { type: "name", value: "add" },
+      { type: "number", value: "2" },
+      { type: "paren", value: "(" },
+      { type: "name", value: "subtract" },
+      { type: "number", value: "4" },
+      { type: "number", value: "2" },
+      { type: "paren", value: ")" },
+      { type: "paren", value: ")" },
+    ]);
+    done();
+  });
 });
-
