43358f8f4aa4e82f6c03522a18655c996f6f5173
gomjellie <gomjellie@gmail.com>
2021-04-18 21:28:24 +0900

    lex를 {, [, ( 같은 여닫는 문자, 알파벳, 문자열, 그외의것으로 분류해서 토큰화하도록 수정함

--- test/js-test.js
+++ test/js-test.js
@@ -3,6 +3,7 @@
  * test files on examples directory
  */
 // const { expect } = require("chai");
+const { expect } = require("chai");
 const fs = require("fs");
 
 exports.jsTest = () => describe('js', function () {
@@ -13,19 +14,27 @@ exports.jsTest = () => describe('js', function () {
     const tokens = lostTemple.tokenizer(source);
 
     console.dir(tokens, { depth: null });
-    
-    // expect(tokens).to.eql([
-    //   { type: 'paren', value: '(' },
-    //   { type: "name", value: "add" },
-    //   { type: "number", value: "2" },
-    //   { type: "paren", value: "(" },
-    //   { type: "name", value: "subtract" },
-    //   { type: "number", value: "4" },
-    //   { type: "number", value: "2" },
-    //   { type: "paren", value: ")" },
-    //   { type: "paren", value: ")" },
-    // ]);
+    const str = tokens.reduce((acc, v) => {
+      return acc + v.value;
+    }, "");
+    console.log(str);
+
+    const unknownTypedToken = tokens.filter((token) => token.type === '???');
+    expect(unknownTypedToken).to.eql([]);
     done();
   });
 
+  it("tests arrow.js tokenize", function (done) {
+    const source = fs.readFileSync(`${__dirname}/sources/arrow.js`).toString();
+    const tokens = lostTemple.tokenizer(source);
+
+    console.dir(tokens, { depth: null });
+    const str = tokens.reduce((acc, v) => {
+      return acc + v.value;
+    }, "");
+    console.log(str);
+    const unknownTypedToken = tokens.filter((token) => token.type === '???');
+    expect(unknownTypedToken).to.eql([]);
+    done();
+  });
 });
