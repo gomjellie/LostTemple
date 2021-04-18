e74e02dc3f8956be179d23c6af4fae61a0db1a51
gomjellie <gomjellie@gmail.com>
2021-04-19 00:36:14 +0900

    multiline, singleline comment 처리

--- test/js-test.js
+++ test/js-test.js
@@ -2,41 +2,40 @@
 /**
  * test files on examples directory
  */
-// const { expect } = require("chai");
 const { expect } = require("chai");
 const fs = require("fs");
 
 exports.jsTest = () => describe('js', function () {
   const lostTemple = require('../lib');
 
-  // it("tests gugu.js tokenize", function (done) {
-  //   const source = fs.readFileSync(`${__dirname}/sources/gugu.js`).toString();
-  //   const tokens = lostTemple.tokenizer(source);
+  it("tests gugu.js tokenize", function (done) {
+    const source = fs.readFileSync(`${__dirname}/sources/gugu.js`).toString();
+    const tokens = lostTemple.tokenizer(source);
 
-  //   console.dir(tokens, { depth: null });
-  //   const str = tokens.reduce((acc, v) => {
-  //     return acc + v.value;
-  //   }, "");
-  //   console.log(str);
+    console.dir(tokens, { depth: null });
+    const str = tokens.reduce((acc, v) => {
+      return acc + v.value;
+    }, "");
+    console.log(str);
 
-  //   const unknownTypedToken = tokens.filter((token) => token.type === '???');
-  //   expect(unknownTypedToken).to.eql([]);
-  //   done();
-  // });
+    const unknownTypedToken = tokens.filter((token) => token.type === '???');
+    expect(unknownTypedToken).to.eql([]);
+    done();
+  });
 
-  // it("tests arrow.js tokenize", function (done) {
-  //   const source = fs.readFileSync(`${__dirname}/sources/arrow.js`).toString();
-  //   const tokens = lostTemple.tokenizer(source);
+  it("tests arrow.js tokenize", function (done) {
+    const source = fs.readFileSync(`${__dirname}/sources/arrow.js`).toString();
+    const tokens = lostTemple.tokenizer(source);
 
-  //   console.dir(tokens, { depth: null });
-  //   const str = tokens.reduce((acc, v) => {
-  //     return acc + v.value;
-  //   }, "");
-  //   console.log(str);
-  //   const unknownTypedToken = tokens.filter((token) => token.type === '???');
-  //   expect(unknownTypedToken).to.eql([]);
-  //   done();
-  // });
+    console.dir(tokens, { depth: null });
+    const str = tokens.reduce((acc, v) => {
+      return acc + v.value;
+    }, "");
+    console.log(str);
+    const unknownTypedToken = tokens.filter((token) => token.type === '???');
+    expect(unknownTypedToken).to.eql([]);
+    done();
+  });
 
   it("tests comment.js tokenize", function (done) {
     const source = fs.readFileSync(`${__dirname}/sources/comment.js`).toString();
